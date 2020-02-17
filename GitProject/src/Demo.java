1a9f6818c87a142da12ba30b690153e921a218b8
IRG <IRG@HP>
2020-02-17 11:28:57 +0530

    change

--- GitProject/src/Demo.java
+++ GitProject/src/Demo.java
@@ -4,7 +4,8 @@ public class Demo
 	public static void main(String args[])
 	{
 		
-      System.out.println("demo");		
+      System.out.println("demo");	
+      System.out.println("hello");
 		
 	}
 
